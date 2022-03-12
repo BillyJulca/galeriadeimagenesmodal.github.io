a4f8d1fc7f63a2555b4e17d7761ae9f6d95088c7
Billy Julca Salvador <julca.17fb@gmail.com>
2022-03-12 13:10:58 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DomContentLoaded', function (){
+
+    let contador = 0 
+    const contenedor = document.querySelector('.slideshow')
+    const overlay = document.querySelector('.overlay')
+    const img_slideshow = document.querySelector('.slideshow img')
+
+document.querySelector('.btn-cerrar').addEventListener('click', () => {
+    overlay.style.opacity = 0;
+    overlay.style.visibility = 'hidden';
+})
+})
+
